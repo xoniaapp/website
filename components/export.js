e783adcf8f46962b7f8f38e992b113c46fd59a28
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2021-11-13 13:48:02 +0000

    Add files via upload

---
+++ components/export.js
@@ -0,0 +1,5 @@
+export { default as Navbar } from "./NavbarContainer/Navbar";
+export { default as Header } from "./HeaderContainer/Header";
+export { default as Feature } from "./FeaturesContainer/Feature";
+export { default as Content } from "./ContentContainer/Content";
+export { default as Footer } from "./FooterContainer/Footer";
---
+++ pages/404.js
@@ -0,0 +1,71 @@
+import Head from "next/head";
+
+export default function () {
+  return (
+    <div>
+      <Head>
+        <title>End to End Encrypted Chat for everyone by Xonia</title>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="keywords" content="xoniaapp.com, xoniaapp" />
+        <meta
+          name="title"
+          content="End to End Encrypted Chat for everyone by Xonia"
+        />
+        <meta
+          name="description"
+          content="Xonia | End to End Encrypted Chat for everyone"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://xoniaapp.com/" />
+        <meta
+          property="og:title"
+          content="End to End Encrypted Chat for everyone by Xonia"
+        />
+        <meta
+          property="og:description"
+          content="Xonia | End to End Encrypted Chat for everyone"
+        />
+        <meta
+          property="og:image"
+          content="https://cdn.xoniaapp.com/static/logo.png"
+        />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://xoniaapp.com/" />
+        <meta
+          property="twitter:title"
+          content="End to End Encrypted Chat for everyone by Xonia"
+        />
+        <meta
+          property="twitter:description"
+          content="Xonia | End to End Encrypted Chat for everyone"
+        />
+        <meta
+          property="twitter:image"
+          content="https://cdn.xoniaapp.com/static/logo.png"
+        />
+        <title>Xonia | End to End Encrypted Chat for everyone</title>
+        <link rel="shortcut icon" href="./logo.png" type="image/x-icon" />
+        <link
+          rel="icon"
+          type="image/x-icon"
+          href="https://cdn.xoniaapp.com/static/logo.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="https://cdn.xoniaapp.com/static/logo.png"
+        />
+        <link rel="manifest" href="./manifest.json" />
+      </Head>
+      <section className="body__wrapper">
+        <center>
+          <h1>
+            404 Page wasn't found. <br />
+          </h1>
+        </center>
+      </section>
+    </div>
+  );
+}
