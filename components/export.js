2ac1157f1601b5dddab33496c8dae5f0def9376b
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2021-11-13 13:52:59 +0000

    Update export.js

--- components/export.js
+++ components/export.js
@@ -3,3 +3,5 @@ export { default as Header } from "./HeaderContainer/Header";
 export { default as Feature } from "./FeaturesContainer/Feature";
 export { default as Content } from "./ContentContainer/Content";
 export { default as Footer } from "./FooterContainer/Footer";
+
+// will be needed later
